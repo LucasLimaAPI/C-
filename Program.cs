8535d518981d3e34f423123a18f21edd52d0683f
Lucas Lima De Oliveira <lucas.oliveira@futuraonline.com.br>
2024-04-15 18:00:14 -0300

    C#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,36 @@
 ﻿// Harmony Sounds
 string welcomeMessage = "Welcome to HarmonySounds !!"; //Camel Case In String
-Console.WriteLine(welcomeMessage);
+
+
+void LetWelcomeMessage()// In C#, the void modifier is used to indicate that a function does not return any value. This means that we want to execute a block of code, but we do not expect to receive anything back as a result of the function's execution.
+{
+    Console.WriteLine(@"
+    ＨａｒｍｏｎｙＳｏｕｎｄｓ
+    ");//"This is a verbatim string literal. It preserves line breaks and escape characters like \n and \t.
+    Console.WriteLine(welcomeMessage);
+} 
+
+void MenuOptions()
+{
+    Console.WriteLine("\nPress 1: Register a new Band");
+    Console.WriteLine("Press 2: Show all bands");
+    Console.WriteLine("Press 3: Rate a band ");
+    Console.WriteLine("Press 4: Display the average of a band ");
+    Console.WriteLine("Press -1: Exit");
+
+    Console.WriteLine("\n Select your option:");
+    string selectOption = Console.ReadLine()!;//! the statement demonstrates that we are not working with null values.
+    int selectOptionNumeric = int.Parse(selectOption);  //int.Parse convert to int
+
+    if (selectOptionNumeric == 1);
+    {
+        Console.WriteLine("You entered the option "+ selectOption);
+    }
+
+
+}
+
+
+
+LetWelcomeMessage();
+MenuOptions();
--- obj/Debug/net8.0/C-.AssemblyInfo.cs
+++ obj/Debug/net8.0/C-.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C-")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c5b542e810f5d41325861b11a5acdebcb95a0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6deedbd5606b9b0a98a482bd338aab92ed3d3f28")]
 [assembly: System.Reflection.AssemblyProductAttribute("C-")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C-")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
